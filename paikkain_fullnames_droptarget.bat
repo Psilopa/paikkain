90b1a39a842b11e7ba44af96a7c0ebe81d059ce6
Jere Kahanpää <54532067+Psilopa@users.noreply.github.com>
2024-03-08 10:38:54 +0200

    Update paikkain_fullnames_droptarget.bat

--- paikkain_fullnames_droptarget.bat
+++ paikkain_fullnames_droptarget.bat
@@ -1,7 +1,7 @@
 chcp 65001 
-"C:\Python373\python.exe" "Z:/paikkain/src/paikkain.py" "Z:/paikkain/config/paikkain_fullnames.toml" %*@echo off
+"C:\Python373\python.exe" "Z:/paikkain/src/paikkain.py" "Z:/paikkain/config/paikkain_fullnames.toml" %*
+@echo off
 set /p id="Enter to quit"
-
 
 REM Set encoding of this file to UTF-8
 REM run Python script
