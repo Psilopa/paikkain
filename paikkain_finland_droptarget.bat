9a2c4a260966936d69b3083e8c81f872b7aeba27
Jere Kahanpää <54532067+Psilopa@users.noreply.github.com>
2024-03-08 10:38:38 +0200

    Update paikkain_finland_droptarget.bat

--- paikkain_finland_droptarget.bat
+++ paikkain_finland_droptarget.bat
@@ -3,11 +3,6 @@ chcp 65001
 @echo off
 set /p id="Enter to quit"
 
-
 REM Set encoding of this file to UTF-8
 REM run Python script
 REM ask for permission to close window
-
-
-
-The name of the institution  (museum, research institute etc). If the institution has several names, the preferred one should be used here
