c2464c87e85b396bce56f055d01a312c56d21522
Jere Kahanpää <54532067+Psilopa@users.noreply.github.com>
2025-10-28 15:30:26 +0200

    Update paikkain_persondata_droptarget.bat  to Python 3.11.5

--- paikkain_persondata_droptarget.bat
+++ paikkain_persondata_droptarget.bat
@@ -1,5 +1,5 @@
 chcp 65001 
-"C:\Python373\python.exe" "Z:/paikkain/src/paikkain.py" "Z:/paikkain/config/paikkain_persondata.toml" %*
+"C:\Python3115\python.exe" "Z:/paikkain/src/paikkain.py" "Z:/paikkain/config/paikkain_persondata.toml" %*
 @echo off
 set /p id="Enter to quit"
 
